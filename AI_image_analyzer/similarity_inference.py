097e7eb8fdfca4d118ab2b9003efb40c03d1d1ae
Josie Harrison <josie@chalmers.se>
2024-06-07 21:01:19 +0200

    Added color info to AI analyzer result

--- AI_image_analyzer/similarity_inference.py
+++ AI_image_analyzer/similarity_inference.py
@@ -5,8 +5,32 @@ import torchvision.transforms as T
 from transformers import AutoModel, AutoFeatureExtractor
 import torch
 import random
+import os
+from PIL import Image 
+import numpy as np
 
 def similarity_inference(directory):
+    
+    # Get color values for each component
+    color_dict = {}
+    for each_image in os.listdir(directory):
+        image_path = os.path.join(directory, each_image)
+        with Image.open(image_path) as img:
+            width, height = img.size
+            # add 50 random color values to color list
+            colors = []
+            for i in range(100):
+                # choose random pixel
+                random_x = random.randint(0, width - 1)
+                random_y = random.randint(0, height - 1)
+                random_pixel = img.getpixel((random_x, random_y))
+                # if pixel is not white
+                if random_pixel != (255, 255, 255): 
+                    colors.append(random_pixel)
+            colors_array = np.array(colors)
+            average_color_value = tuple(np.mean(colors_array, axis=0).astype(int))
+            color_dict[each_image] = average_color_value
+
     convert_images_to_grayscale(directory)
     crop_center_largest_contour(directory)
 
@@ -38,17 +62,22 @@ def similarity_inference(directory):
     label_filenames = ds.load_dataset("imagefolder", data_dir=directory).cast_column("image", ds.Image(decode=False))
 
     # loop through each component and return top 3 most similar
-    match_dict = {"ceiling": [], "floor": [], "wall": []}
+    match_dict = {"ceiling": [], 
+                  "floor": [], 
+                  "wall": []}
     for i, each_component in enumerate(test_ds['train']):
         query_image = each_component["image"]
-        component_label = label_filenames['train'][i]['image']['path'].split('_')[-1]
-        print(component_label)
-        match = re.search(r"([a-zA-Z]+)\d*\.png", component_label)
+        component_label = label_filenames['train'][i]['image']['path'].split('_')[-1].split("\\")[-1]
+        rgb_color = color_dict[component_label]
+        match = re.search(r"([a-zA-Z]+)(\d*)\.png", component_label)
         component_label = match.group(1)
+        segment_id = match.group(2)
         sim_ids = fetch_similar(query_image, transformation_chain, device, model, all_candidate_embeddings, candidate_ids)
         for each_match in sim_ids:
+            component_texture_id = str(segment_id) + "-" + str(each_match)
             texture_filename = candidate_subset_emb[each_match]['filenames']
             image_url = f'https://cdn.polyhaven.com/asset_img/thumbs/{texture_filename}?width=256&height=256'
-            match_dict[component_label].append(image_url)
+            temp_dict = {"id": component_texture_id, "thumbnail": image_url, "name": texture_filename, "color": str(rgb_color)}
+            match_dict[component_label].append(temp_dict)
 
     return match_dict
